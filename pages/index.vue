a439b12a77b34350d2f824e1b0e4ecf688fba4eb
Dirta Putra Anggara <dirtaputraanggara@gmail.com>
2022-02-28 21:12:23 +0700

    feat(fetch-data): fetch pict data from backend

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,57 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >discord</a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >issue board</a>.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em>
-              <small>&mdash; John Leider</small>
-            </em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer">Nuxt Documentation</a>
+  <div>
+    <nav class="navbar navbar-light sticky-top bg-light">
+      <a class="navbar-brand" href="#">Flickr - Feed</a>
+    </nav>
+
+    <div class="container-fluid">
+      <!-- Tab panes -->
+      <div class="tab-content">
+        <div id="home" class="container-fluid tab-pane active">
           <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Nuxt GitHub</a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+          <div class="row justify-content-center">
+            <div class>
+              <div class="card mx-auto custom-card" id="prova" v-for="feed of feed" :key="feed">
+                <div class="row post-header col-12 py-2 px-3">
+                  <div class="col-6 float-left">
+                    <h4>{{feed.title}}</h4>
+                  </div>
+                  <div class="col-6 float-right text-right post-number">
+                    <h4>{{feed.author}}</h4>
+                  </div>
+                </div>
+                <img class="card-img" :src="feed.pict" width="700 px" />
+                <div class="card-body px-3">
+                  <h5 class="card-title">Date: {{feed.published}}</h5>
+                </div>
+                <div class="row post-header px-3 pb-3">
+                  <div class="col-1 float-left text-left">
+                    <i class="far fa-heart"></i>
+                  </div>
+                  <div class="col-1 float-right text-right">
+                    <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-import VuetifyLogo from "~/components/VuetifyLogo.vue";
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data() {
+    return {
+      feed: []
+    };
+  },
+  async fetch() {
+    this.feed = await fetch(
+      "https://vast-thicket-64154.herokuapp.com/getFeed"
+    ).then(res => res.json());
   }
 };
 </script>
